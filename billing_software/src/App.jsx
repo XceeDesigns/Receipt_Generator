5b60384d86e874a3f12a05a4cf83b28f934152b1
Akshat Trivedi <akshattrivedi32@gmail.com>
2024-11-12 23:28:15 +0530

    dashboard created

--- billing_software/src/App.jsx
+++ billing_software/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import SignUp from './components/SignUp'
 import SignIn from './components/SignIn'
+import Dashboard from './components/Dashboard'
 
 function App() {
 
   return (
     <>
     {/* <SignUp/> */}
-    <SignIn/>
+    {/* <SignIn/> */}
+    <Dashboard/>
     </>
   )
 }
