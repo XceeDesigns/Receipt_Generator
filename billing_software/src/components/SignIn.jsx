86f768a2a9f3cf40e86dc695b414495103c15b37
Akshat Trivedi <akshattrivedi32@gmail.com>
2024-11-15 19:46:27 +0530

    link updated

--- billing_software/src/components/SignIn.jsx
+++ billing_software/src/components/SignIn.jsx
@@ -21,7 +21,7 @@ export default function SignInPage() {
       return;
     }
 
-    const response = await fetch("http://localhost:8080/api/user/login", {
+    const response = await fetch(`${backend_url}/api/user/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
